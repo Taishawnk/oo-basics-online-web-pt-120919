315a77748d2deea23635f8b13cf296e1a6b4c360
Taishawn King <taishawnking1@yahoo.com>
2020-01-01 01:15:10 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,4 @@
 class Shoe
-<<<<<<< HEAD
   attr_accessor :color,:size,:material,:condition
   attr_reader :brand
 def initialize(brand)
@@ -11,14 +10,5 @@ end
 end
 end
 
-=======
-  attr_accessor :brand, :color,:size,:material,:condition
-def initialize(brand)
-  @brand=brand
-  def cobble
-    puts 
-end
-end
-end
->>>>>>> 5a7f2e775a38cc0fd16d69d082e694d9bc97fcb6
+
 
